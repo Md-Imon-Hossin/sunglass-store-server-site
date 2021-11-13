ee35e8c031e4cd270a6e6a30b8392372ed9f8cbe
imon hossin <imonhossin104@gmail.com>
2021-11-13 10:52:25 +0600

    first

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require('mongodb').ObjectId ;
 const app = express()
 const port =process.env.PORT || 5000
 
-// middlewaree 
+// middleware 
 app.use(cors())
 app.use(express.json())
 
